769d59ba68ff53349bfd0a2b3158067f77367aec
fragmepls <114432515+fragmepls@users.noreply.github.com>
2023-02-02 16:42:41 +0100

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
+import { BookStoreServiceTestComponent } from './book-store-service-test/book-store-service-test.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, BookStoreServiceTestComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/book-store-service-test/book-store-service-test.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bs-book-store-service-test',
+  templateUrl: './book-store-service-test.component.html',
+  styleUrls: ['./book-store-service-test.component.scss'],
+})
+export class BookStoreServiceTestComponent {
+}
